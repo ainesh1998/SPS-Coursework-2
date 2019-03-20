7ca4b92986b9441e94ad152a0a652e48591a7a60
M-Mafeni <mm17291@my.bristol.ac.uk>
2019-03-20 23:34:35 +0000

    implemented feature selection, added 13 by 13 feature scatter plot

--- wine_classifier.py
+++ wine_classifier.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Skeleton code for CW2 submission. 
+Skeleton code for CW2 submission.
 We suggest you implement your code in the provided functions
 Make sure to use our print_features() and print_predictions() functions
 to print your results
@@ -18,36 +18,59 @@ from utilities import load_data, print_features, print_predictions
 CLASS_1_C = r'#3366ff'
 CLASS_2_C = r'#cc3300'
 CLASS_3_C = r'#ffc34d'
+class_colours = [CLASS_1_C,CLASS_2_C,CLASS_3_C]
 
-MODES = ['feature_sel', 'knn', 'alt', 'knn_3d', 'knn_pca']    
-
-
+MODES = ['feature_sel', 'knn', 'alt', 'knn_3d', 'knn_pca']
+#     Helper Functions     #
+def getColours(index,train_set):
+    onlyColours = []
+    for i in range(len(train_labels)):
+        if(train_labels[i] == index + 1):
+            onlyColours.append(train_set[i])
+    arr = np.array(onlyColours)
+    return arr
+################################
 def feature_selection(train_set, train_labels, **kwargs):
-    # write your code here and make sure you return the features at the end of 
+    # write your code here and make sure you return the features at the end of
     # the function
-    return []
+    # scatter plot bit
+    n_features = train_set.shape[1]
+    fig, ax = plt.subplots((n_features), (n_features))
+    plt.subplots_adjust(left=0.01, right=0.99, top=0.99, bottom=0.01, wspace=0.2, hspace=0.4)
+    for i in range(len(class_colours)):
+        for j in range(n_features):
+            for k in range(n_features):
+                arr = getColours(i,train_set)
+                if(len(arr) != 0):
+                    ax1.scatter(arr[:,j],arr[:,k],c = class_colours[i])
+                    ax[j,k].xaxis.set_visible(False)
+                    ax[j,k].yaxis.set_visible(False)
+                    ax[j,k].scatter(arr[:,j],arr[:,k],c = class_colours[i])
+    plt.show()
+    return [12,10]
 
 
 def knn(train_set, train_labels, test_set, k, **kwargs):
-    # write your code here and make sure you return the predictions at the end of 
+    # write your code here and make sure you return the predictions at the end of
     # the function
+    # nearest centroid bit
     return []
 
 
 def alternative_classifier(train_set, train_labels, test_set, **kwargs):
-    # write your code here and make sure you return the predictions at the end of 
+    # write your code here and make sure you return the predictions at the end of
     # the function
     return []
 
 
 def knn_three_features(train_set, train_labels, test_set, k, **kwargs):
-    # write your code here and make sure you return the predictions at the end of 
+    # write your code here and make sure you return the predictions at the end of
     # the function
     return []
 
 
 def knn_pca(train_set, train_labels, test_set, k, n_components=2, **kwargs):
-    # write your code here and make sure you return the predictions at the end of 
+    # write your code here and make sure you return the predictions at the end of
     # the function
     return []
 
@@ -60,18 +83,18 @@ def parse_args():
     parser.add_argument('--train_labels_path', nargs='?', type=str, default='data/wine_train_labels.csv', help='Path to training labels')
     parser.add_argument('--test_set_path', nargs='?', type=str, default='data/wine_test.csv', help='Path to the test set csv')
     parser.add_argument('--test_labels_path', nargs='?', type=str, default='data/wine_test_labels.csv', help='Path to the test labels csv')
-    
+
     args = parser.parse_args()
     mode = args.mode[0]
-    
+
     return args, mode
 
 
 if __name__ == '__main__':
     args, mode = parse_args() # get argument from the command line
-    
+
     # load the data
-    train_set, train_labels, test_set, test_labels = load_data(train_set_path=args.train_set_path, 
+    train_set, train_labels, test_set, test_labels = load_data(train_set_path=args.train_set_path,
                                                                        train_labels_path=args.train_labels_path,
                                                                        test_set_path=args.test_set_path,
                                                                        test_labels_path=args.test_labels_path)
